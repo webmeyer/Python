2f9dfe90cce416dec07998f68c22fe5935d733df
webmeyer <meyerwork22@gmail.com>
2020-10-26 06:57:17 +0500

    Add dir "Sequence" with sequence.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-#Example
+#Последовательности
 
-using_list.py
+shoplist = ['яблоки', 'манго', 'морковь', 'бананы']
+name = 'what_a_wonderful_world'
+
+#Операция индексирования
+print('Элемент 0 -', shoplist[0])
+print('Элемент 1 -', shoplist[1])
+print('Элемент 2 -', shoplist[2])
+print('Элемент 3 -', shoplist[3])
+print('Элемент -1 -', shoplist[-1])
+print('Элемент -2 -', shoplist[-3])
+print('Символ 0 -', name[0])
+
+#Вырезка из списка
+print('Элементы с 1 по 3 -', shoplist[1:3])
+print('Элементы с 2 до конца -', shoplist[2:])
+print('Элементы с 1 по -1: ', shoplist[1:-1])
+print('Элементы от начала до конца', shoplist[:])
+
+#Вырезка из строки
+print('Символы с 1 по 3 -', name[1:3])
+print('Символы со 2 до конца -', name[2:])
+print('Символы с 1 по -1: ', name[1:-1])
+print('Символы от начала до конца', name[:])
+
