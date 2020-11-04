c349a7ce288327ade111dace439a1f0b5f0f808d
webmeyer <meyerwork22@gmail.com>
2020-11-04 19:18:53 +0500

    update elif_basic.py

--- Stepik/elif_basic.py
+++ Stepik/elif_basic.py
@@ -161,3 +161,38 @@ else:
 
 
 # Задание 8
+# На числовой прямой даны два отрезка: a и b.
+# Напишите программу, которая находит их пересечение.
+
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
+
+if(a1 < a2 < b2 < b1):
+    print(a2, b2)
+elif(a1 < a2 < b1 < b2):
+    print(a2, b1)
+elif(a2 < a1 < b2 < b1):
+    print(a1, b2)
+elif(a2 < a1 < b1 < b2):
+    print(a1, b1)
+elif(a1 < b1 == a2 < b2):
+    print(b1)
+elif(a2 < b2 == a1 < b1):
+    print(a1)
+elif(a1 == a2 < b1 == b2):
+    print(a1, b1)
+elif(a1 == a2 < b1 < b2):
+    print(a1, b1)
+elif(a2 < a1 < b1 == b2):
+    print(a1, b1)
+elif(a1 < a2 < b1 == b2):
+    print(a2, b1)
+elif(a1 == a2 < b2 < b1):
+    print(a2, b2)
+else:
+    if(a1 < b1 < a2 < b2):
+        print('пустое множество')
+    elif(a2 < b2 < a1 < b1):
+        print('пустое множество')
--- main.py
+++ main.py
@@ -1,29 +1,5 @@
 #Example
 
-num = int(input())
 
-if num in range(0, 37, 1):
-    if(num == 0):
-        print('зеленый')
-    elif(num in range(1, 11, 1)):
-        if(num % 2 == 0):
-            print('черный')
-        else:
-            print('красный')
-    elif (num in range(11, 19, 1)):
-        if (num % 2 == 0):
-            print('красный')
-        else:
-            print('черный')
-    elif (num in range(19, 29, 1)):
-        if (num % 2 == 0):
-            print('черный')
-        else:
-            print('красный')
-    elif (num in range(29, 37, 1)):
-        if (num % 2 == 0):
-            print('красный')
-        else:
-            print('черный')
-else:
-    print('ошибка ввода')
+
+
