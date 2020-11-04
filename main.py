cd08f43a9c65fe0fd89a5921b909ad36b1876fcc
webmeyer <meyerwork22@gmail.com>
2020-11-04 17:59:16 +0500

    update elif_basic.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 #Example
 
+color1 = input()
+color2 = input()
+
+if(color1 == 'красный' and color2 == 'красный'):
+    print('красный')
+elif(color1 == 'синий' and color2 == 'синий'):
+    print('синий')
+elif(color1 == 'желтый' and color2 == 'желтый'):
+    print('желтый')
+else:
+    if((color1 == 'красный' and color2 == 'желтый') or (color1 == 'желтый' and color2 == 'красный')):
+        print('оранжевый')
+    elif((color1 == 'красный' and color2 == 'синий') or (color1 == 'синий' and color2 == 'красный')):
+        print('фиолетовый')
+    elif((color1 == 'синий' and color2 == 'желтый') or (color1 == 'желтый' and color2 == 'синий')):
+        print('зеленый')
+    else:
+        print('ошибка цвета')
