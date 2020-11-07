26b5a47a684e7a30520881f2c58ea23db879f4be
webmeyer <meyerwork22@gmail.com>
2020-11-07 14:05:27 +0500

    upd data_type.py

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 #Example
 
-num1 = int(input())
+p1 = int(input())
+p2 = int(input())
 
-a = num1 % 10
-b = num1 % 100
-b = b // 10
-c = num1 / 100
-c = c % 10
+q1 = int(input())
+q2 = int(input())
 
-minValue = min(a, b, c)
-maxValue = max(a, b, c)
-middle = (a + b + c) - (maxValue + minValue)
+firstPoint = abs(p1 - q1)
+secondPoint = abs(p2 - q2)
 
-print(int(minValue))
-print(int(maxValue))
-print(int(middle))
-
-if(int(middle) == int(maxValue) - int(minValue)):
-    print('Число интересное')
-else:
-    print('Число неинтересное')
+print(firstPoint + secondPoint)
