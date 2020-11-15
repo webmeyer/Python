95a326dce501061af197c17d882c0a67ddc23bf8
webmeyer <meyerwork22@gmail.com>
2020-11-15 18:47:04 +0500

    add task #9

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 #Example
 
+m = int(input())
+n = int(input())
 
+if(m < n):
+    for i in range(m, n+1):
+        print(i)
+elif(m > n):
+    for i in range(m, n-1, -1):
+        print(i)
+else:
+    print("Числа равны:", m)
