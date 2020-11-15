97ea1e689d464a382c3bc8f63eecbfe886147d31
webmeyer <meyerwork22@gmail.com>
2020-11-15 18:44:30 +0500

    add task #9

--- Practice/Stepik/cycles.py
+++ Practice/Stepik/cycles.py
@@ -115,3 +115,21 @@ n = int(input())
 
 for i in range(m, n+1):
     print(i)
+
+
+
+# Задание 9
+# Даны два целых числа m и n (m ≤ n).
+# Напишите программу, которая выводит все числа от m до n включительно.
+
+m = int(input())
+n = int(input())
+
+if(m < n):
+    for i in range(m, n+1):
+        print(i)
+elif(m > n):
+    for i in range(m, n-1, -1):
+        print(i)
+else:
+    print(m)
