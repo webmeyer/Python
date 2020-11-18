95022a9e0f4d2e4efcd4e892d62eb507aceae648
webmeyer <meyerwork22@gmail.com>
2020-11-18 19:20:18 +0500

    add task #15

--- Practice/Stepik/cycles.py
+++ Practice/Stepik/cycles.py
@@ -219,3 +219,21 @@ for i in range(n):
     number = int(input())
     summ += number
 print(summ)
+
+
+
+# Задание 15
+# На вход программе подается натуральное число n.
+# Напишите программу, которая вычисляет значение выражения: (1 + (1 / 2) + (1 / 3) + ... + (1 / n)) -ln(n).
+
+import math
+
+n = int(input())
+summ = 0
+
+for i in range(n + 1):
+    if i != 0:
+        drob = (1 / i)
+        summ += drob
+result = summ - math.log(n)
+print(result)
