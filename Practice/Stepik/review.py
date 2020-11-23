8b3d96edfc359a9e0449ca7dcb734c2559ee192a
webmeyer <meyerwork22@gmail.com>
2020-11-23 11:23:39 +0500

    add task #5

--- Practice/Stepik/review.py
+++ Practice/Stepik/review.py
@@ -50,3 +50,22 @@ print(s)
 
 # Задание 4
 # Найти ошибки в коде
+
+n = int(input())
+max_digit = -1
+while n > 0:
+    digit = n % 10
+    if digit % 3 == 0:
+        if digit > max_digit:
+            max_digit = digit
+    n = n // 10
+if max_digit == -1:
+    print('NO')
+else:
+    print(max_digit)
+
+
+
+# Задание 5
+# Найти ошибки в коде
+
