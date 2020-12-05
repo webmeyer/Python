6da17700690d58c18a98eda0e538a44a81c42b3a
webmeyer <meyerwork22@gmail.com>
2020-12-05 14:01:13 +0500

    add task #3

--- Practice/Stepik/string_methods.py
+++ Practice/Stepik/string_methods.py
@@ -19,3 +19,21 @@ else:
 str1 = input()
 print(str1.swapcase())
 
+
+
+# Задание 3
+# Дополните приведенный код, используя форматирование строк с помощью метода format, так чтобы он вывел текст:
+# «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
+
+year = 2010
+price = str('10k')
+coin = 'Bitcoin'
+s = 'In {0}, someone paid {1} {2} for two pizzas.'.format(year, price, coin)
+
+print(s)
+
+
+
+# Задание 4
+# Дополните приведенный код, используя форматирование строк с помощью метода format, так чтобы он вывел текст:
+# «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
