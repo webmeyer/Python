47d556dab29e2d2d14425c7d0f308e56687af145
webmeyer <meyerwork22@gmail.com>
2020-12-09 21:53:53 +0500

    add task#7

--- Practice/Stepik/list.py
+++ Practice/Stepik/list.py
@@ -83,3 +83,15 @@ for i in range(len(abc)):
     abc_list.append(abc[i] * count)
     count += 1
 print(abc_list)
+
+
+
+# Задача 7
+# Дополните приведенный код, так чтобы он вывел сумму квадратов элементов списка numbers.
+
+numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+sum = 0
+for i in range(len(numbers)):
+    quatro = (numbers[i] * numbers[i])
+    sum += quatro
+print(sum)
