7a901ab7dee688781da30312df105cf9c172a8f4
webmeyer <meyerwork22@gmail.com>
2020-12-11 20:52:44 +0500

    add task#9

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #Example
 
-abc = 'abcdefghijklmnopqrstuvwxyz'
-abc_list = []
-count = 1
+n = int(input())
+books = []
 
-for i in range(len(abc)):
-    abc_list.append(abc[i] * count)
-    count += 1
-print(abc_list)
+while n > 0:
+    abc = input()
+    n -= 1
+
+k = int(input())
+for i in range(n):
+    books = books.append([k])
+print(books)
