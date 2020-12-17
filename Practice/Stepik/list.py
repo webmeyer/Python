925b88031a07841d969f58ef13283ac9e07720cf
webmeyer <meyerwork22@gmail.com>
2020-12-17 08:48:39 +0500

    add task #30

--- Practice/Stepik/list.py
+++ Practice/Stepik/list.py
@@ -465,3 +465,13 @@ keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'cl
 
 lengths = [len(word) for word in keywords]
 print(lengths)
+
+
+
+# Задача 30
+# Дополните приведенный код списочным выражением, чтобы получить новый список, содержащий только слова длиной не менее пяти символов (включительно).
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords = [word for word in keywords if len(word) >= 5]
+print(new_keywords)
