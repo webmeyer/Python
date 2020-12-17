7dfaeb649cebad37811db5f6a66dc23e53c1937c
webmeyer <meyerwork22@gmail.com>
2020-12-17 08:44:26 +0500

    add task #29

--- Practice/Stepik/list.py
+++ Practice/Stepik/list.py
@@ -455,3 +455,13 @@ keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'cl
 
 new_keywords = [m[1:] for m in keywords]
 print(new_keywords)
+
+
+
+# Задача 29
+# Дополните приведенный код, используя списочное выражение, так чтобы получить новый список, содержащий длины строк исходного списка.
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+lengths = [len(word) for word in keywords]
+print(lengths)
