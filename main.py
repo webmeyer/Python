7a8e012482992a236dbbf084c9f910b6ce37dcfe
webmeyer <meyerwork22@gmail.com>
2020-12-17 12:54:46 +0500

    upd

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-#Example
+# Example
 
-n = int(input())
-books = []
-
-while n > 0:
-    abc = input()
-    n -= 1
-
-k = int(input())
-for i in range(n):
-    books = books.append([k])
-print(books)
