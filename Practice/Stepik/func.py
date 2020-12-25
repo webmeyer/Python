ae0608b05c22f80419e054b7d1ef2ab51385f1b6
webmeyer <meyerwork22@gmail.com>
2020-12-25 09:09:18 +0500

    add task#4

--- Practice/Stepik/func.py
+++ Practice/Stepik/func.py
@@ -36,3 +36,24 @@ def draw_box(width, height):
         print('*' * height)
 
 draw_box(10, 14)
+
+
+
+# Задача 4
+# Напишите функцию draw_triangle(fill, base), которая принимает два параметра:
+#
+# fill – символ заполнитель;
+# base – величина основания равнобедренного треугольника;
+# а затем выводит его.
+
+def draw_triangle(fill, base):
+    a = list(range(1, base // 2 + 1 + 1)) + list(range(base // 2, 0, -1))
+    for i in a:
+        print(fill * i)
+
+# считываем данные
+fill = input()
+base = int(input())
+
+# вызываем функцию
+draw_triangle(fill, base)
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # Example
 
-# Задача 3
-# Напишите функцию
-def draw_box(width, height):
-    for i in range(width):
-        print('*' * height)
+def draw_triangle(fill, base):
+    a = list(range(1, base // 2 + 1 + 1)) + list(range(base // 2, 0, -1))
+    for i in a:
+        print(fill * i)
 
-draw_box(10, 14)
+# считываем данные
+fill = input()
+base = int(input())
+
+# вызываем функцию
+draw_triangle(fill, base)
