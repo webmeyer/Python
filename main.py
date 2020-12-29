92a72f69c8a5b92fec2a91555713b3f513225843
webmeyer <meyerwork22@gmail.com>
2020-12-29 12:31:31 +0500

    add task#13

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-# Example
+def quick_merge(list1, list2):
+    result = []
+    p1 = 0  # указатель на первый элемент списка list1
+    p2 = 0  # указатель на первый элемент списка list2
+    while p1 < len(list1) and p2 < len(list2):  # пока не закончился хотя бы один список
+        if list1[p1] <= list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+        else:
+            result.append(list2[p2])
+            p2 += 1
+    if p1 < len(list1):   # прицепление остатка
+        result += list1[p1:]
+    if p2 < len(list2):
+        result += list2[p2:]
+    return result
 
-# объявление функции
-def merge(list1, list2):
-    return sorted(list1 + list2)
-
-# считываем данные
+n = int(input())
 numbers1 = [int(c) for c in input().split()]
-numbers2 = [int(c) for c in input().split()]
-
-# вызываем функцию
-print(merge(numbers1, numbers2))
+for _ in range(1, n):
+    numbers1 = quick_merge(numbers1, [int(c) for c in input().split()])
+print(*numbers1)
