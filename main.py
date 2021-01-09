168d68db473d52cb83c6b3fba748794e2aa8d25a
webmeyer <meyerwork22@gmail.com>
2021-01-09 13:22:39 +0500

    add task#18

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
-# word1 = input()
-# word2 = input()
-# count = 0
-#
-# if len(word1) == len(word2):
-#      for i in word1:
-#          print(i)
-#          for j in word2:
-#             if i == j:
-#                 count += 1
-# if count == len(word1) - 1:
-#     print(count)
-# else:
-#     print('Отличаются больше, чем на 1 символ')
-# считываем данные
-
+# EXAMPLE
 
 # объявление функции
-def is_one_away(word1, word2):
-    count = 0
-    if len(word1) == len(word2):
-        for i in word1:
-            for j in word2:
-                if i == j:
-                    count += 1
-        if count == len(word1) - 1 or count - 1 == len(word1):
-            return True
-        else:
-            return False
+def is_palindrome(text):
+    txt_list = []
+    for char in text:
+        if char.isalpha():
+            txt_list.append(char.lower())
+    if txt_list[:] == txt_list[::-1]:
+        return True
     else:
         return False
 
 # считываем данные
-txt1 = input()
-txt2 = input()
+txt = input()
 
 # вызываем функцию
-print(is_one_away(txt1, txt2))
+print(is_palindrome(txt))
 
 
 
