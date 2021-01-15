92b44cc35756aa3de2168f095fa6477f74a26d67
webmeyer <meyerwork22@gmail.com>
2021-01-15 11:37:16 +0500

    add task#1

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # EXAMPLE
 
-# объявление функции
-from math import factorial
-def compute_binom(n, k):
-    bino1 = factorial(n)
-    bino2 = factorial(k)*factorial(n-k)
-    return bino1//bino2
-
-# считываем данные
-n = int(input())
-k = int(input())
-
-# вызываем функцию
-print(compute_binom(n, k))
+import random
+print('Сыграем в "Угадай число"?')
+user_name = input('Введите Ваше имя..')
+num_generate = random.randint(1, 100)
+while True:
+    user_num = int(input(f'{user_name}, введите число от 1 до 100 \n'))
+    if user_num > num_generate:
+        print('Слишком много, попробуйте ещё раз')
+    elif user_num < num_generate:
+        print('Маловато, попробуйте ещё раз')
+    else:
+        print('Поздравляем! Вы угадали!')
+        break
 
 
 
