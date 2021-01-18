a24fe89f42bd8c34abf53e44e9f373cb604349d3
webmeyer <meyerwork22@gmail.com>
2021-01-18 20:44:31 +0500

    upd

--- Projects/ugadaika.py
+++ Projects/ugadaika.py
@@ -33,13 +33,13 @@ def is_valid(num):
 
 while True:
     response = input('Введите число от 1 до 100:')
-    if not is_valid(responce):
+    if not is_valid(response):
         print('А может быть все-таки введем целое число от 1 до 100?')
         continue
     response = int(response)
-    if val < number:
+    if response < number:
         print('Ваше число меньше загаданного, попробуйте еще разок')
-    elif val > number:
+    elif response > number:
         print('Ваше число больше загаданного, попробуйте еще разок')
     else:
         print('Вы угадали, поздравляем!')
